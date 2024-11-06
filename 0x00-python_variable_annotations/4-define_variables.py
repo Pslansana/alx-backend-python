21c571e1a92f8103e1e0ee71f0a14f19a7e6711d
Peter Sylvester Lansana <Lansanapeters@gmail.com>
2024-11-06 23:48:44 +0000

    Define variables

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" This is a module that define the data type of each variable """
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
