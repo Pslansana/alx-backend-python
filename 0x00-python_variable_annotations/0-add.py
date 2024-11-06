b982a7c2a8981e2980004f1167dd2c4a77c3f820
Peter Sylvester Lansana <Lansanapeters@gmail.com>
2024-11-06 23:38:01 +0000

    Basic annotations - add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" This is a method that add up two floats and return the value float """
+
+
+def add(a: float, b: float) -> float:
+    """ This is a function that return the sum of a and b """
+    return a + b
