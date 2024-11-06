526e65b1683de6c023dd28afcd9463ed79d7e2b7
Peter Sylvester Lansana <Lansanapeters@gmail.com>
2024-11-06 23:44:27 +0000

    Basic annotations - floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+This is a module that pass the argument floor and return value floor
+"""
+
+
+import math
+
+
+def floor(n: float) -> int:
+    """ This return the vaue n as a floor """
+    return math.floor(n)
